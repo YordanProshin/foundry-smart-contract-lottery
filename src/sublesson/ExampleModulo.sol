fae8930b6a81187270bf05826926a85aafb8f99f
YordanProshin <yordan.proshin@gmail.com>
2024-10-08 12:29:19 +0300

    Foundry-Lottery

---
+++ src/sublesson/ExampleModulo.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract ExampleModulo {
+    function getModTen(uint256 number) external pure returns (uint256) {
+        
+        return number % 10;
+    }
+
+    function getModTwo(uint256 number) external pure returns (uint256) {
+        return number % 2;
+    }
+}
